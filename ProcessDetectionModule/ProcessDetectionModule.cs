8e8c69556b66ead29eac51638056a452a39ffdfe
Xynous <day.ashleigh@gmail.com>
2016-09-30 23:03:41 +0100

    Cleaned up
    Cleaned up code with comments and also redone the logical if structure

--- ProcessDetectionModule/ProcessDetectionModule.cs
+++ ProcessDetectionModule/ProcessDetectionModule.cs
@@ -14,10 +14,7 @@ namespace NS_ProcessDetectionModule
 {
     class ProcessDetectionModule
     {
-        public static void Main()
-        {
-            ProcessDetection();
-        }
+        
         public static void ProcessDetection()
         {
             // Infinite for loop which will constanly loop to check the client.
@@ -26,9 +23,9 @@ namespace NS_ProcessDetectionModule
                 #region Internal (Injectors)
 
                 // Injector Strings goes here what processes you want to detect.
-                Process[] PC1I = Process.GetProcessesByName("");
-                Process[] PC2I = Process.GetProcessesByName("notepad");
-                Process[] PC3I = Process.GetProcessesByName("notepad");
+                Process[] PC1I = Process.GetProcessesByName("Process you want to detect goes here");
+                Process[] PC2I = Process.GetProcessesByName("Process you want to detect goes here");
+                Process[] PC3I = Process.GetProcessesByName("Process you want to detect goes here");
 
                 #endregion
 
@@ -36,53 +33,53 @@ namespace NS_ProcessDetectionModule
 
                 // External application strings goes here what processes you want to detect.
 
-                Process[] PC1E = Process.GetProcessesByName("notepad");
-                Process[] PC2E = Process.GetProcessesByName("notepad");
-                Process[] PC3E = Process.GetProcessesByName("notepad");
+                Process[] PC1E = Process.GetProcessesByName("Process you want to detect goes here");
+                Process[] PC2E = Process.GetProcessesByName("Process you want to detect goes here");
+                Process[] PC3E = Process.GetProcessesByName("Process you want to detect goes here");
 
                 #endregion
 
                 #region Internal Checks
 
-                // If the cheat is not running, do nothing
+                // if the process array stores 1 element, then execute this if statement. In this case it is holding 1 element since the starting element starts at 1.
                 if (PC1I.Length == 1)
                 {
                     // Passes the process name string into process1 variable
-                    foreach (var process1 in Process.GetProcessesByName("sublime_text"))
+                    foreach (var process1 in Process.GetProcessesByName("Process goes here to kill"))
                     {
                         // Kills game process or whatever process you passed and set to kill. So if the process is detected running (which is set above), then kill this process what is set here (if its running). 
                         process1.Kill();
 
-                        // Puts a 1 ms sleep on the thread to prevent exception error after process is killed.
+                        // Puts a 1 second sleep on the thread to prevent exception error after process is killed.
                         Thread.Sleep(1000);
                     }
                 }
 
-                // If the cheat is running, perform the following action/task.
+                // if the process array stores 1 element, then execute this if statement. In this case it is holding 1 element since the starting element starts at 1.
                 else if (PC2I.Length == 1)
                 {
                     // Passes the process name string into process1 variable
-                    foreach (var process1 in Process.GetProcessesByName("sublime_text"))
+                    foreach (var process1 in Process.GetProcessesByName("Process goes here to kill"))
                     {
                         // Kills game process or whatever process you passed and set to kill. So if the process is detected running (which is set above), then kill this process what is set here (if its running).
                         process1.Kill();
 
-                        // Puts a 1 ms sleep on the thread to prevent exception error after process is killed.
+                        // Puts a 1 second sleep on the thread to prevent exception error after process is killed.
                         Thread.Sleep(1000);
                     }
                 }
 
 
-                // If the cheat is running, perform the following action/task.
+                // if the process array stores 1 element, then execute this if statement. In this case it is holding 1 element since the starting element starts at 1.
                 else if (PC3I.Length == 1)
                 {
                     // Passes the process name string into process1 variable
-                    foreach (var process1 in Process.GetProcessesByName("sublime_text"))
+                    foreach (var process1 in Process.GetProcessesByName("Process goes here to kill"))
                     {
                         // Kills game process or whatever process you passed and set to kill. So if the process is detected running (which is set above), then kill this process what is set here (if its running).
                         process1.Kill();
 
-                        // Puts a 1 ms sleep on the thread to prevent exception error after process is killed.
+                        // Puts a 1 second sleep on the thread to prevent exception error after process is killed.
                         Thread.Sleep(1000);
                     }
 
@@ -90,7 +87,7 @@ namespace NS_ProcessDetectionModule
 
                 else
                 {
-                    // Do nothing
+                    // Do nothing, will execute when all other if statements are false.
 
                 }
 
@@ -98,51 +95,51 @@ namespace NS_ProcessDetectionModule
 
                 #region External Checks
 
-                // If the cheat is not running, do nothing
+                // if the process array stores 1 element, then execute this if statement. In this case it is holding 1 element since the starting element starts at 1.
                 if (PC1E.Length == 1)
                 {
                     // Passes the process name string into process1 variable
-                    foreach (var process1 in Process.GetProcessesByName("sublime_text"))
+                    foreach (var process1 in Process.GetProcessesByName("Process goes here to kill"))
                     {
                         // Kills game process or whatever process you passed and set to kill. So if the process is detected running (which is set above), then kill this process what is set here (if its running).
                         process1.Kill();
 
-                        // Puts a 1 ms sleep on the thread to prevent exception error after process is killed.
+                        // Puts a 1 second sleep on the thread to prevent exception error after process is killed.
                         Thread.Sleep(1000);
                     }
                 }
 
-                // If the cheat is running, perform the following action/task.
+                // if the process array stores 1 element, then execute this if statement. In this case it is holding 1 element since the starting element starts at 1.
                 else if (PC2E.Length == 1)
                 {
                     // Passes the process name string into process1 variable
-                    foreach (var process1 in Process.GetProcessesByName("sublime_text"))
+                    foreach (var process1 in Process.GetProcessesByName("Process goes here to kill"))
                     {
                         // Kills game process or whatever process you passed and set to kill. So if the process is detected running (which is set above), then kill this process what is set here (if its running).
                         process1.Kill();
 
-                        // Puts a 1 ms sleep on the thread to prevent exception error after process is killed.
+                        // Puts a 1 second sleep on the thread to prevent exception error after process is killed.
                         Thread.Sleep(1000);
                     }
                 }
 
-                // If the cheat is running, perform the following action/task.
+                // if the process array stores 1 element, then execute this if statement. In this case it is holding 1 element since the starting element starts at 1.
                 else if (PC3E.Length == 1)
                 {
                     // Passes the process name string into process1 variable
-                    foreach (var process1 in Process.GetProcessesByName("sublime_text"))
+                    foreach (var process1 in Process.GetProcessesByName("Process goes here to kill"))
                     {
                         // Kills game process or whatever process you passed and set to kill. So if the process is detected running (which is set above), then kill this process what is set here (if its running).
                         process1.Kill();
 
-                        // Puts a 1 ms sleep on the thread to prevent exception error after process is killed.
+                        // Puts a 1 second sleep on the thread to prevent exception error after process is killed.
                         Thread.Sleep(1000);
                     }
                 }
 
                 else
                 {
-                    // Do nothing.
+                    // Do nothing. will execute when all other if statements are false.
                 }
 
                 #endregion
